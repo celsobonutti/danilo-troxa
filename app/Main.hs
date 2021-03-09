297755c4f8a549deb62dc59dbbcb082eafed455a
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2021-03-08 21:51:05 -0300

    less do, more >>=

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,14 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [filename] -> do
-      csv <- readFile filename
-      case CSV.parse csv of
-        Nothing ->
-          putStrLn "Error parsing CSV"
-        Just parsedCSV ->
-          putStrLn $ Json.fromCSV parsedCSV
-
+    [filename] ->
+      readFile filename >>= printCsv
     _ ->
       putStrLn "Incorrect number of arguments"
+
+printCsv :: String -> IO ()
+printCsv csv = case CSV.parse csv of
+  Nothing ->
+    putStrLn "Error parsing CSV"
+  Just parsedCSV ->
+    putStrLn $ Json.fromCSV parsedCSV
